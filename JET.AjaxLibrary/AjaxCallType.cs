c1ab26b7b672a23240933f23b9c1a3c3a4000e87
jetwind <f-windchina@hotmail.com>
2013-07-09 23:05:59 +0800

    update uccontrols bug
    更新了用户控件执行器的bug

--- JET.AjaxLibrary/AjaxCallType.cs
+++ JET.AjaxLibrary/AjaxCallType.cs
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-=======
-﻿
-using System;
->>>>>>> 2013-05-18
+﻿using System;﻿
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,29 +6,24 @@ using System.Text;
 namespace JET.AjaxLibrary
 {
     /// <summary>
-<<<<<<< HEAD
     /// ajax请求的类型
-=======
     /// The ajax call the type of request(ajax请求的类型:类方法,)
->>>>>>> 2013-05-18
     /// </summary>
     public enum AjaxCallType
     {
-        /// <summary>
-<<<<<<< HEAD
-        /// 用户自定义后缀请求
-        /// </summary>
-        HttpUserDefHandle,
+        ///// <summary>
+        ///// 用户自定义后缀请求
+        ///// </summary>
+        //HttpUserDefHandle,
+
         /// <summary>
         /// ascx控件请求
-=======
         /// The ajax call the type of request,like:Order.Add.cs (用户自定义后缀的请求,如直接请求cs类中的方法)
         /// </summary>
         HttpUserDefHandle,
 
         /// <summary>
         /// Request user control (请求用户控件,Order.ascx)
->>>>>>> 2013-05-18
         /// </summary>
         HttpUcHandle
     }
